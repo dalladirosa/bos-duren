93b1fee9bf3a41c85bf40d5b762e435b12890e95
unknown <dalladirosa@gmail.com>
2018-08-31 11:18:02 +0700

    Update express,css,html

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const publicPath = path.join(__dirname, '..', 'public')
 
 app.use(express.static(publicPath))
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'))
+})
+
 app.listen(3000, () => {
   console.log('Server is up!')
 })
